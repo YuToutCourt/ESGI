d326271f791109a11af76c6ef58cbbe37328817f
Yù <wapzif@gmail.com>
2022-12-08 08:49:02 +0100

    end python

--- Python/Tp1/all_exo.py
+++ Python/Tp1/all_exo.py
@@ -45,13 +45,12 @@ def somme_facotrielle():
 
         number = int(input("Entrer un entier positif : "))
 
-        somme, factorielle = 0, 1
+        somme, factorielle = 0, math.factorial(number)
         number_used_in_str = []
         
         # Calcul de la somme et de la factorielle
         for i in range(1, number + 1):
             somme += i
-            factorielle *= i
             number_used_in_str.append(str(i))
 
         # Affichage de la somme
@@ -97,7 +96,89 @@ def math_():
     print(f"cos({entier}) = {math.cos(entier)}")
 
 
-def res(x:int, n:int):
+def f1(x: float, n: int) -> float:
     """
-    J'ai pas compris faut faire quoi
+    Calcul de la fonction f1(x,n) = x^n / n!
+    :param x: float
+    :param n: int
+
+    :return: float
     """
+    return x ** n / math.factorial(n)
+
+
+
+def Res(x: float, N: int) -> float:
+    """
+    Calcul de la somme de f1(x,n) pour n allant de 1 à N
+    :param x: float
+    :param N: int
+
+    :return: float
+    """
+    somme = 0
+    # Calcul de la somme de f1(x,n) pour n allant de 1 à N
+    for n in range(1, N+1):
+        somme += f1(x, n)
+    return somme
+
+
+
+def U(n: int) -> float:
+    """
+    Calcul de la suite U
+    :param n: int
+
+    :return: float
+    """
+    u = 1
+    
+    # Calcul des termes de la suite
+    for i in range(1, n+1):
+        u += 1 / math.factorial(i)
+    
+    return u
+
+def V(n: int) -> float:
+    """
+    Calcul de la suite V
+    :param n: int
+
+    :return: float
+    """
+    v = 0
+
+    # Calcul des termes de la suite
+    for i in range(1, n+1):
+        v += U(i) + 1 / (i * math.factorial(i))
+    
+    return v
+
+# Variable globale contenant les valeurs déjà calculées de la factorielle pour éviter de les recalculer
+fact = [1]
+def tierce():
+    """
+    Calcul de la probabilité de gagner au tiercé, quarté, quinté...
+    """
+
+    # Calcul de la factorielle d'un nombre entier en utilisant la programmation dynamique
+    def factoriel(n: int) -> int:
+        # Si la factorielle a déjà été calculée, on la retourne directement
+        if n < len(fact):
+            return fact[n]
+        
+        # Sinon, on calcule la factorielle et on l'ajoute au tableau
+        res = n *  math.factorial(n-1)
+        fact.append(res)
+        return res
+
+    # Demande du nombre de chevaux partants et de chevaux joués à l'utilisateur
+    n = int(input("Nombre de chevaux partants : "))
+    p = int(input("Nombre de chevaux joués : "))
+
+    X = factoriel(n) // factoriel(n-p)
+    Y = factoriel(n) // (factoriel(p) * factoriel(n-p))
+    
+    # Affichage des chances de gagner
+    print(f"Une chance sur {int(X)} de gagner dans l'ordre")
+    print(f"Une chance sur {int(Y)} de gagner dans le désordre")
