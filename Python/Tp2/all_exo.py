2bb107952f51282228992536f17694397ac1fcc1
Yù <wapzif@gmail.com>
2023-01-11 12:38:46 +0100

    fichiers

--- Python/Tp2/all_exo.py
+++ Python/Tp2/all_exo.py
@@ -1,3 +1,5 @@
+import os
+
 from random import randint
 
 def calcul_surface():
@@ -41,5 +43,47 @@ def jeu_allumettes():
 
         nb_allumettes = nb_allumettes - nb_remove
         turn += 1
+    # TODO : To finish
 
-        
+def fichiers():
+    nombres = list(map(int, input("Entrer une liste de nombres séparés par des espaces : ").split()))
+
+    def create(file_name: str, data:list[int], binary=False):
+        """
+        Creates a file with the given name, and writes the data into it.
+        :param file_name: str
+        :param data: list[int]
+        :param binary: bool
+        """
+        mode = "wb" if binary else "w"
+        with open(file_name, mode) as f:
+            for d in data:
+                if binary:
+                    f.write(d.to_bytes(4, byteorder="big"))
+                else:
+                    f.write(str(d) + "\n")
+
+    def read(file_name:str, binary=False):
+        """
+        Reads the given file, and prints the content of the file.
+        :param file_name: str
+        :param binary: bool
+        """
+        mode = "rb" if binary else "r"
+        with open(file_name, mode) as f:
+            while True:
+                if binary:
+                    d = f.read(4)
+                    if not d: break
+                    print(int.from_bytes(d, byteorder="big"))
+                else:
+                    d = f.readline()
+                    if not d: break
+                    print(int(d))
+
+    create("BDD.bin", nombres, binary=True)
+    read("BDD.bin", binary=True)
+    create("BDD.txt", nombres, binary=False)
+    read("BDD.txt", binary=False)
+
+
