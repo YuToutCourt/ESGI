e7be18b1f93a39ed6c1ada014f2eb85d1022edcc
Yù <wapzif@gmail.com>
2023-03-06 14:00:37 +0100

    Controle algo

---
+++ Algo/exo.py
@@ -0,0 +1,50 @@
+"""
+Author:  Yohann MALEY
+Date:    06-03-2022
+ESGI 3
+"""
+
+import math
+
+
+def exo_1():
+    pass
+
+def exo_2():
+    pass
+
+def exo_3():
+    pass
+
+def exo_4():
+    pass
+
+def exo_5():
+    pass
+
+def exo_6():
+    pass
+
+def exo_7():
+    pass
+
+def exo_8():
+    pass
+
+def exo_9():
+    pass
+
+def exo_10():
+    pass
+
+if __name__ == "__main__":
+    # exo_1()
+    # exo_2()
+    # exo_3()
+    # exo_4()
+    # exo_5()
+    # exo_6()
+    # exo_7()
+    # exo_8()
+    # exo_9()
+    # exo_10()
